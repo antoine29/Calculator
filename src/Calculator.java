03e15d2731e10522d8753de98dab786f0bb5504a
Antoine <anthonyfg9@gmail.com>
2019-09-30 00:41:03 -0400

    Adding basic actions

--- src/Calculator.java
+++ src/Calculator.java
@@ -70,41 +70,152 @@ public class Calculator {
         btnC.setBounds(x[0],y[1],widthBtn,heightBtn);
         btnC.setFont(btnFont);
         btnC.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnC.addActionListener(event -> {
+            display.setText("0");	//erase the display setting a zero
+            operation = ' ';
+            val = 0;
+        });
+
         window.add(btnC);
                 
         btnBack = new JButton("<-");
         btnBack.setBounds(x[1],y[1],widthBtn,heightBtn);
         btnBack.setFont(btnFont);
         btnBack.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnBack.addActionListener(event -> {
+            String str = display.getText();
+            String str2 = "";
+            for (int i=0; i<str.length()-1; i++) str2+=str.charAt(i);
+            if (str2.equals("")) display.setText("0");
+            else display.setText(str2);
+        });
         window.add(btnBack);
         
         btnMod = new JButton("%");
         btnMod.setBounds(x[2],y[1],widthBtn,heightBtn);
         btnMod.setFont(btnFont);
         btnMod.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnMod.addActionListener(event -> {
+            if (isValidDouble(display.getText())) {
+            	if (canOperate) {
+                    val = calc(val, display.getText(), operation);
+                    display.setText(Integer.toString((int) val));
+                    canOperate = false;
+                    canAddDigits = false;
+                }
+                operation = '%';
+            }
+        });
         window.add(btnMod);
         
         btnDiv = new JButton("/");
         btnDiv.setBounds(x[3],y[1],widthBtn,heightBtn);
         btnDiv.setFont(btnFont);
         btnDiv.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnDiv.addActionListener(event -> {
+            if (isValidDouble(display.getText())) {
+            	if (canOperate) {
+                    val = calc(val, display.getText(), operation);
+                    if (isValidInteger(Double.toString(val))) display.setText(Integer.toString((int) val));
+                    else display.setText(Double.toString(val));
+                    canOperate = false;
+                    canAddDigits = false;
+                }
+                operation = '/';
+            }
+        });
         window.add(btnDiv);
         
         btn7 = new JButton("7");
         btn7.setBounds(x[0],y[2],widthBtn,heightBtn);
         btn7.setFont(btnFont);
         btn7.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btn7.addActionListener(event -> {
+            if (canAddDigits) {
+            	if (display.getText().equals("0")) display.setText("7");
+            	else display.setText(display.getText() + "7");
+            }
+            else {
+                display.setText("7");
+                canAddDigits = true;
+            }
+            canOperate = true;
+        });
         window.add(btn7);
         
         btn8 = new JButton("8");
         btn8.setBounds(x[1],y[2],widthBtn,heightBtn);
         btn8.setFont(btnFont);
         btn8.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btn8.addActionListener(event -> {
+            if (canAddDigits) {
+                if (display.getText().equals("0")) display.setText("8");
+                else display.setText(display.getText() + "8");
+            }
+            else {
+                display.setText("8");
+                canAddDigits = true;
+            }
+            canOperate = true;
+        });
         window.add(btn8);
         
         window.setLayout(null);
         window.setResizable(false);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // If Click into The Red Button => End The Calculator process
         window.setVisible(true);
+
+        btnEqual = new JButton("=");
+        btnEqual.setBounds(x[2],y[5],2*widthBtn+10,heightBtn);
+        btnEqual.setFont(btnFont);
+        btnEqual.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnEqual.addActionListener(event -> {
+            if (isValidDouble(display.getText())) {
+            	if (canOperate) {
+                    val = calc(val, display.getText(), operation);
+                    if (isValidInteger(Double.toString(val))) display.setText(Integer.toString((int) val));
+                    else {
+                    	String z = String.valueOf(val);
+                    	if(z.length()>12) z = z.substring(0, 13) + "..."; 
+                        display.setText(z);
+                    }
+                    operation = '=';
+                    canAddDigits = false;
+                }
+            }
+        });
+        window.add(btnEqual);
+    }
+
+    private boolean isValidDouble(String number) {
+    	boolean sw = true;
+    	try {
+    		Double.parseDouble(number);
+    	}
+    	catch(Exception e) {
+    		System.out.println(e.getMessage());
+    		sw = false;
+    	}
+    	
+    	return sw;
+    }
+
+    private boolean isValidInteger(String number) {
+    	boolean sw = true;
+    	
+    	double dNumber = Double.parseDouble(number);
+    	int a = (int) dNumber;
+    	return dNumber == (double)a;
+    }
+
+    private double calc(double x, String input, char operation) {
+        double y = Double.parseDouble(input);
+        switch (operation) {
+		    case '%': return x % y;
+		    case '/': return x / y;
+			default: break;
+		}
+
+        return y;
     }
 }
